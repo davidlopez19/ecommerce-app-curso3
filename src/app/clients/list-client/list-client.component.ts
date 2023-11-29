3e1b57262fa4a89be0a81173d413f6c289c5a24c
mohamed smith <mohara.dip@outlook.es>
2023-11-28 20:12:46 -0500

    jg: modulo de clientes (ini)

--- src/app/clients/list-client/list-client.component.ts
+++ src/app/clients/list-client/list-client.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-client.component.css']
 })
 export class ListClientComponent implements OnInit {
+  clients = []
 
   constructor() { }
 
