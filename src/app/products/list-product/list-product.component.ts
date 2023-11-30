9027110c247dc5c307d3bd05a869a996362a072e
david_lopez <josue.cano@doctus.com.co>
2023-11-29 21:21:21 -0500

    se añade boton

--- src/app/products/list-product/list-product.component.ts
+++ src/app/products/list-product/list-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductModel } from 'src/app/models/products/product.model';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,11 +11,18 @@ import { ProductService } from 'src/app/services/product.service';
 export class ListProductComponent implements OnInit {
   products: Array<ProductModel> = [];
 
-  constructor(private _productService: ProductService) {}
+  constructor(
+    private _productService: ProductService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
     this._productService.getListProducts().subscribe((result) => {
       this.products = result;
     });
+  }
+
+  redirectToCreate() {
+    this._router.navigateByUrl('products-create');
   }
 }
